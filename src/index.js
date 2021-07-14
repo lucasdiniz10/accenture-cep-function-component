c21b9df7eb31cab7db72fd75c0e4853490abf4c9
Lucas Diniz <lucasalvarengadiniz@gmail.com>
2021-07-14 20:17:50 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App name={'Lucas Diniz'} />
   </React.StrictMode>,
   document.getElementById('root')
 );
